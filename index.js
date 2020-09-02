c4b38e0835a14060b76c0d6b2509146a4d697828
github username <github email address>
2020-09-02 09:14:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, func) {
+    let newArray = [];
+    array.forEach(element => {
+        newArray.push(func(element))
+    })
+    return newArray
+}
+
+function reduce(array, func, start) {
+    let a = (!!start) ? start : array[0]
+    let b = (!!start) ? 0 : 1
+
+    for (; b < array.length; b++) {
+        a = func(array[b], a)
+    }
+    return a
+}
